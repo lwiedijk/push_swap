eada0f8c2a9d381cfc2d580a039e6b3ee2436027
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-09-22 13:20:21 +0200

    small stack sort

--- create_stack.c
+++ create_stack.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/26 13:17:40 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/09/15 14:46:33 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/09/22 13:17:55 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,22 +41,73 @@ void	free_list(t_stack *lst)
 	}
 }
 
-int	check_list_is_sorted(t_stack *stack)
+int	list_is_sorted(t_stack *stack)
 {
-	long int compare1;
-	long int compare2;
+	long	int compare1;
+	long	int compare2;
 
 	if (!stack)
-		return (0);
+		return (FALSE);
 	while (stack->next != NULL)
 	{
 		compare1 = stack->to_sort;
 			stack = stack->next;
 		compare2 = stack->to_sort;
 		if (compare1 > compare2)
-			return (0);
+			return (FALSE);
 	}
-	return (1);
+	return (TRUE);
+}
+
+int	top_of_small_stack(t_stack *stack_a)
+{
+	long	int compare1;
+	long	int compare2;
+	long	int compare3;
+
+	if (!stack_a || stack_a->next == NULL)
+		return (DONT_SORT);
+	if (stack_a->next->next == NULL)
+	{
+		sa(stack_a);
+		return (DONT_SORT);
+	}
+	compare1 = stack_a->to_sort;
+	compare2 = stack_a->next->to_sort;
+	compare3 = stack_a->next->next->to_sort;
+	if (compare1 > compare2 && compare1 > compare3)
+		return (HIGHEST);
+	else if (compare1 < compare2 && compare1 < compare3)
+		return (LOWEST);
+	else
+	{
+		if (compare2 < compare3)
+			return (MIDDEL_LOW);
+		else
+			return (MIDDEL_HIGH);
+	}
+}
+
+void	sort_small_stack(t_stack *stack_a)
+{
+	int	stack_top;
+	
+	stack_top = top_of_small_stack(stack_a);
+	if (stack_top == HIGHEST)
+	{
+		ra(&stack_a, FALSE);
+		if (!list_is_sorted(stack_a))
+		sa(stack_a);
+	}
+	if (stack_top == LOWEST)
+	{
+		sa(stack_a);
+		ra(&stack_a, FALSE);
+	}
+	if (stack_top == MIDDEL_LOW)
+		sa(stack_a);
+	if (stack_top == MIDDEL_HIGH)
+		rra(&stack_a, FALSE);
 }
 
 int	main(int ac, char **av)
@@ -91,36 +142,45 @@ int	main(int ac, char **av)
 		}
 		i++;
 	}
+
 	print_stack_list_forward(stack_a, &printf, 'a');
 	print_stack_list_forward(stack_b, &printf, 'b');
-	print_stack_list_backward(stack_a, &printf, 'a');
-	print_stack_list_backward(stack_b, &printf, 'b');
+	//print_stack_list_backward(stack_a, &printf, 'a');
+	//print_stack_list_backward(stack_b, &printf, 'b');
+
+	if (ac < 5)
+	{
+		if (!list_is_sorted(stack_a))
+			sort_small_stack(stack_a);
+	}
+	else
+		printf("need sort large list\n");
 	
 	//sa(stack_a);
 	//sb(stack_b);
 	//ss(stack_a, stack_b);
 	//pa(stack_a, stack_b);
 
-	pb(&stack_a, &stack_b);
-	pb(&stack_a, &stack_b);
-	pb(&stack_a, &stack_b);
-	print_stack_list_forward(stack_a, &printf, 'a');
-	print_stack_list_forward(stack_b, &printf, 'b');
+	//pb(&stack_a, &stack_b);
+	//pb(&stack_a, &stack_b);
+	//pb(&stack_a, &stack_b);
+	//print_stack_list_forward(stack_a, &printf, 'a');
+	//print_stack_list_forward(stack_b, &printf, 'b');
 
 	//ra(&stack_a, FALSE);
 	//rb(&stack_b, FALSE);
 	//rr(&stack_a, &stack_b);
 	//rra(&stack_a, FALSE);
 	//rrb(&stack_b, FALSE);
-	rrr(&stack_a, &stack_b);
+	//rrr(&stack_a, &stack_b);
 
 	print_stack_list_forward(stack_a, &printf, 'a');
 	print_stack_list_forward(stack_b, &printf, 'b');
-	print_stack_list_backward(stack_a, &printf, 'a');
-	print_stack_list_backward(stack_b, &printf, 'b');
+	//print_stack_list_backward(stack_a, &printf, 'a');
+	//print_stack_list_backward(stack_b, &printf, 'b');
 	
 	
-	if (check_list_is_sorted(stack_a))
+	if (list_is_sorted(stack_a))
 		return (printf("list is sorted\n"));
 	return (printf("not sorted or stack_a is empty\n"));
 	
