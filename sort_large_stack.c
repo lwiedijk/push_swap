c1bb3039e64b8adeb8b98ef92243e641d2d1290a
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-10-08 13:48:10 +0200

    put index to stack

--- sort_large_stack.c
+++ sort_large_stack.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/08 09:40:23 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2021/10/08 11:37:53 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2021/10/08 13:43:35 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,35 @@
 
 void	get_index_position(t_stack *stack_a, t_stack *stack_array, int stack_count)
 {
+	t_stack	*temp_head;
+	int		i;
+
+	i = 0;
+	temp_head = stack_a;
+	while (i < stack_count)
+	{
+		temp_head = stack_a;
+		while (temp_head)
+		{
+			if (stack_array[i].to_sort == temp_head->to_sort)
+			{
+				temp_head->index = stack_array[i].index;
+			}
+			temp_head = temp_head->next;
+		}
+		i++;
+	}
+
+}
+
+void	sort_stack_array(t_stack *stack_a, t_stack *stack_array, int stack_count)
+{
 	long int	temp_num;
-	t_stack		*temp_head;
+	//t_stack		*temp_head;
 	int			i;
 	int			j;
 	
-	temp_head = stack_a;
+	//temp_head = stack_a;
 	i = 0;
 	while (i < stack_count)
 	{
@@ -35,9 +58,10 @@ void	get_index_position(t_stack *stack_a, t_stack *stack_array, int stack_count)
 			}
 			j++;
 		}
+		stack_array[i].index = i;
 		i++;
 	}
-
+	get_index_position(stack_a, stack_array, stack_count);
 }
 
 void	put_stack_to_array(t_stack *stack_a, int stack_count)
@@ -59,11 +83,8 @@ void	put_stack_to_array(t_stack *stack_a, int stack_count)
 		i++;
 	}
 	//stack_array[i] = NULL; dit werkt niet want geen node, is 0 termination nodig hier? 
-	get_index_position(stack_a, stack_array, stack_count);
+	sort_stack_array(stack_a, stack_array, stack_count);
 	free(stack_array);
-
-	
-	
 }
 
 void	sort_large_stack(t_stack **stack_a, t_stack **stack_b, int stack_count)
