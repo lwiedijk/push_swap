bc171da5d9a06b5f3ca798d681017df2cadf3d34
Laura Wiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-10-13 11:52:25 +0200

    add a & b instruction

---
+++ a_instructions.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   a_instructions.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/09/13 09:31:11 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2021/10/13 11:17:31 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <unistd.h>
+#include <stdio.h>
+
+void	ra(t_stack **stack_a, int rr_flag)
+{
+	t_stack	*last;
+
+	if (*stack_a && (*stack_a)->next)
+	{
+		last = last_node(*stack_a);
+		last->next = *stack_a;
+		*stack_a = (*stack_a)->next;
+		(*stack_a)->prev = NULL;
+		last->next->next = NULL;
+		last->next->prev = last;
+	}
+	if (!rr_flag)
+		write(1, "ra\n", 3);
+}
+
+void	rra(t_stack **stack_a, int rrr_flag)
+{
+	t_stack	*last;
+
+	if (*stack_a && (*stack_a)->next)
+	{
+		last = last_node(*stack_a);
+		last->next = *stack_a;
+		last->prev->next = NULL;
+		*stack_a = last;
+		(*stack_a)->prev = NULL;
+		(*stack_a)->next->prev = *stack_a;
+	}
+	if (!rrr_flag)
+		write(1, "rra\n", 4);
+}
+
+void	sa(t_stack *stack_a)
+{
+	long int	temp;
+
+	if (stack_a->next)
+	{
+		temp = stack_a->to_sort;
+		stack_a->to_sort = stack_a->next->to_sort;
+		stack_a->next->to_sort = temp;
+	}
+	write(1, "sa\n", 3);
+}
+
+void	pa(t_stack **stack_a, t_stack **stack_b,
+			t_stack *temp_a, t_stack *temp_b)
+{
+	if (*stack_b)
+	{
+		if ((*stack_b)->next)
+			temp_b = (*stack_b)->next;
+		*stack_a = *stack_b;
+		if ((*stack_b)->next)
+			*stack_b = temp_b;
+		else
+			*stack_b = NULL;
+		if (temp_a)
+		{
+			(*stack_a)->next = temp_a;
+			(*stack_a)->next->prev = *stack_a;
+		}
+		else
+			(*stack_a)->next = NULL;
+		if (*stack_b)
+			(*stack_b)->prev = NULL;
+		(*stack_a)->prev = NULL;
+	}
+	write(1, "pa\n", 3);
+}
